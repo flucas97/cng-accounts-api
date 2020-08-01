d2b882513424452031a0cb108f868baee03bc5fc
flucas97 <feijolucas1997@gmail.com>
2020-08-01 19:58:31 -0300

    implement create account login

--- domain/accounts/accounts_dao.go
+++ domain/accounts/accounts_dao.go
@@ -1,24 +1,49 @@
 package accounts
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/flucas97/CNG-checknogreen/account/db/postgres/accounts_db"
-	"github.com/flucas97/CNG-checknogreen/account/utils/errors_utils"
+	"github.com/flucas97/CNG-checknogreen/account/utils/crypto"
+	"github.com/flucas97/CNG-checknogreen/account/utils/date"
+
 	"github.com/flucas97/CNG-checknogreen/account/utils/logger"
+	"github.com/flucas97/bookstore/users-api/utils/error_factory"
 )
 
 const (
 	queryCreateAccount = ("INSERT INTO accounts(name, language, password, country, city, description) VALUES (?, ?, ?, ?, ?, ?);")
+	statusActive       = "active"
+	statusEnded        = "ended"
 )
 
 // Create Account
-func (account *Account) Create() {
+func (account *Account) Create() *error_factory.restErr {
 	stmt, err := accounts_db.Client.Prepare(queryCreateAccount)
 	if err != nil {
 		logger.Error("error while preparing Create query", err)
-		return errors_utils.NewInternalServerError("an error occurred while creating account. Try again")
+		return errors.NewInternalServerError("an error occurred while creating account. Try again")
 	}
 	defer stmt.Close()
 
+	account.CreatedAt, account.UpdatedAt, account.Status = date.GetNowString(), date.GetNowString(), statusActive
+	account.Password = crypto.GetMd5(account.Password)
+
+	insertResult, err := stmt.Exec(account.Name, account.Language, account.Password, account.Country, account.City, account.Description, account.Status, account.CreatedAt, account.UpdatedAt)
+	if err != nil {
+		if strings.Contains(err.Error(), "Duplicate entry") {
+			return error_factory.NewBadRequestError(fmt.Sprintf("account '%v' already exists", account.Name))
+		}
+	}
+	accountID, err := insertResult.LastInsertId()
+	if err != nil {
+		logger.Error("error while saving account in database query", err)
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save account: %s", err.Error()))
+	}
+
+	account.ID = accountID
+	return nil
 }
 
 // Update Account
