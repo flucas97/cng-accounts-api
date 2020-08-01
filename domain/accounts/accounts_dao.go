baf00fc84fc7e4b2c1bed96f9d7913aed5cfd5d3
flucas97 <feijolucas1997@gmail.com>
2020-08-01 19:15:51 -0300

    init accounts dao

---
+++ domain/accounts/accounts_dao.go
@@ -0,0 +1,31 @@
+package accounts
+
+// Create Account
+func (account *Account) Create() {
+
+}
+
+// Update Account
+func (account *Account) Update() {
+
+}
+
+// Login Account
+func (account *Account) Login() {
+
+}
+
+// Validate Account
+func (account *Account) Validate() {
+
+}
+
+// Delete Account
+func (account *Account) Delete() {
+
+}
+
+// Freeze Account
+func (account *Account) Freeze() {
+
+}
