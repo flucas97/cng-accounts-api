e79c7c4baef5ce1dd971103dfcc8e803f3debf6f
flucas97 <feijolucas1997@gmail.com>
2020-08-01 19:10:27 -0300

    add comments about exported function

--- app/routes.go
+++ app/routes.go
@@ -2,6 +2,7 @@ package app
 
 import "github.com/flucas97/CNG-checknogreen/account/controllers/ping"
 
+// Routes map all avaliable routes
 func Routes() {
 	router.GET("/ping", ping.Ping)
 }
--- controllers/ping/ping_controller.go
+++ controllers/ping/ping_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Ping check application status
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
