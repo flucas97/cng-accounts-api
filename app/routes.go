f49b36a25936cff933c463ae4f20c35375b8a581
flucas97 <feijolucas1997@gmail.com>
2020-08-02 01:28:00 -0300

    fix route login method

--- app/routes.go
+++ app/routes.go
@@ -11,5 +11,5 @@ func Routes() {
 
 	// accounts
 	router.POST("/new-account", accounts_controller.Create)
-	router.GET("/login", accounts_controller.Login)
+	router.POST("/login", accounts_controller.Login)
 }
