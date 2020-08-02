2b730c33f37c7d99b8694c045bba60d98d76ca2d
flucas97 <feijolucas1997@gmail.com>
2020-08-01 21:33:51 -0300

    implement serivce feature, db connection OK

--- domain/accounts/accounts_dao.go
+++ domain/accounts/accounts_dao.go
@@ -2,51 +2,37 @@ package accounts
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/flucas97/CNG-checknogreen/account/db/postgres/accounts_db"
 	"github.com/flucas97/CNG-checknogreen/account/utils/crypto"
 	"github.com/flucas97/CNG-checknogreen/account/utils/date"
 	"github.com/flucas97/CNG-checknogreen/account/utils/error_factory"
-	"github.com/flucas97/CNG-checknogreen/account/utils/logger"
 )
 
 const (
-	queryCreateAccount = ("INSERT INTO accounts(name, email, language, password, country, city, description) VALUES (?, ?, ?, ?, ?, ?, ?);")
+	queryCreateAccount = ("INSERT INTO accounts (name, email, language, password, country, city, description, status, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;")
 	statusActive       = "active"
 	statusEnded        = "ended"
 )
 
 // Create Account
 func (account *Account) Create() *error_factory.RestErr {
+	var err error
 	// check if is everything OK accessing the DB
-	if err := accounts_db.Client.Ping(); err != nil {
+	if err = accounts_db.Client.Ping(); err != nil {
 		panic(err)
 	}
-
-	stmt, err := accounts_db.Client.Prepare(queryCreateAccount)
-	if err != nil {
-		logger.Error("error while preparing Create query", err)
-		return error_factory.NewInternalServerError("an error occurred while creating account. Try again")
-	}
-	defer stmt.Close()
 
 	account.CreatedAt, account.UpdatedAt, account.Status = date.GetNowString(), date.GetNowString(), statusActive
 	account.Password = crypto.GetMd5(account.Password)
 
-	insertResult, err := stmt.Exec(account.Name, account.Email, account.Language, account.Password, account.Country, account.City, account.Description, account.Status, account.CreatedAt, account.UpdatedAt)
+	id := 0
+	err = accounts_db.Client.QueryRow(queryCreateAccount, account.Name, account.Email, account.Language, account.Password, account.Country, account.City, account.Description, account.Status, account.CreatedAt, account.UpdatedAt).Scan(&id)
 	if err != nil {
-		if strings.Contains(err.Error(), "Duplicate entry") {
-			return error_factory.NewBadRequestError(fmt.Sprintf("account '%v' already exists", account.Name))
-		}
-	}
-	accountID, err := insertResult.LastInsertId()
-	if err != nil {
-		logger.Error("error while saving account in database query", err)
-		return error_factory.NewInternalServerError(fmt.Sprintf("error when trying to save account: %s", err.Error()))
+		return error_factory.NewBadRequestError(fmt.Sprintf("error querying row %v", err))
 	}
 
-	account.ID = accountID
+	account.ID = int64(id)
 	return nil
 }
 
