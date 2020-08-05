0d9831b7215f6316091fb1f21fc36b6a4e36df19
flucas97 <feijolucas1997@gmail.com>
2020-08-05 11:40:52 -0300

    clear and fix sql schema file

--- db/postgres/schema/accounts.sql
+++ db/postgres/schema/accounts.sql
@@ -1,7 +1,7 @@
 -- public.account definition
 -- Drop table
-DROP TABLE IF EXISTS public.account;
-CREATE TABLE public.account (
+DROP TABLE IF EXISTS account;
+CREATE TABLE account (
     id serial primary key,
     name text not null unique,
     email text not null unique,
@@ -17,24 +17,3 @@ CREATE TABLE public.account (
     updated_at timestamp NOT NULL DEFAULT now(),
     created_at timestamp NOT NULL DEFAULT now()
 );
-
-CREATE OR REPLACE VIEW public.metrics
-  AS SELECT
-    account.id,
-    account.name,
-    account.email,
-    account.password,
-    account.city,
-    account.country,
-    account.state,
-    account.avaliable_features,
-    account.cannabis_repository_id,
-    account.description,
-    account.language,
-    account.status,
-    account.updated_at,
-    account.created_at
-   FROM account;
-
-ALTER TABLE public.account OWNER TO postgres;
-GRANT ALL ON TABLE public.account TO postgres;
